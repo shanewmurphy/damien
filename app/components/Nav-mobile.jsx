facf2713fc481b712cd3a37d5c493244d5af790f
Shane Murphy <shn.murphy@gmail.com>
2023-12-12 11:34:27 +0000

    Add mobile menu

--- app/components/Nav-mobile.jsx
+++ app/components/Nav-mobile.jsx
@@ -1,9 +1,13 @@
+import MobileMenu from "./Mobile-menu";
+
 export default function NavBarMobile() {
   return (
     <div className="flex m-4 justify-between items-center mobile-nav">
       <div className="font-bold text-3xl">Logo</div>
       <div>
-        <button className="bg-black text-white p-2">Get A Quote</button>
+        <button className="bg-black text-white p-2">
+          <MobileMenu />
+        </button>
       </div>
     </div>
   );
