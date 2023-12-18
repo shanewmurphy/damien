8c5fb13183114c3acd933a563a748458da28fa5c
Shane Murphy <shn.murphy@gmail.com>
2023-12-18 10:56:52 +0000

    About Us page

---
+++ app/About/page.js
@@ -0,0 +1,76 @@
+export default function AboutUsPage() {
+  return (
+    <div>
+      <div className="items-stretch bg-white flex w-full flex-col justify-center px-16 py-12 max-md:max-w-full max-md:px-5">
+        <div className="mt-16 mb-10 max-md:max-w-full max-md:mr-1 max-md:mt-10">
+          <div className="gap-5 flex max-md:flex-col max-md:items-stretch max-md:gap-0">
+            <div className="flex flex-col items-stretch w-6/12 max-md:w-full max-md:ml-0">
+              <div className="items-stretch flex flex-col max-md:max-w-full max-md:mt-10">
+                <div className="text-black text-center text-base font-semibold leading-6 max-md:max-w-full">
+                  Innovative
+                </div>
+                <div className="text-black text-6xl font-bold leading-[67px] mt-4 max-md:max-w-full max-md:text-4xl max-md:leading-[54px]">
+                  Revolutionizing Underfloor Heating
+                </div>
+              </div>
+            </div>
+            <div className="flex flex-col items-stretch w-6/12 ml-5 max-md:w-full max-md:ml-0">
+              <div className="items-stretch flex grow flex-col max-md:max-w-full max-md:mt-10">
+                <div className="text-black text-lg leading-7 max-md:max-w-full">
+                  Welcome to our company, where we are dedicated to innovating
+                  in the field of underfloor heating systems. With a rich
+                  history of expertise and a commitment to excellence, we strive
+                  to provide the most comfortable, efficient, and eco-friendly
+                  heating solutions for your home or business.
+                </div>
+                <div className="items-stretch flex gap-4 mt-6 pt-4 self-start">
+                  <div className="text-black text-base leading-6 whitespace-nowrap justify-center items-stretch border grow px-6 py-3 border-solid border-black max-md:px-5">
+                    Contact Us
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className="items-stretch bg-white flex w-full flex-col justify-center px-16 py-12 max-md:max-w-full max-md:px-5">
+          <div className="mt-16 mb-10 max-md:max-w-full max-md:mr-1 max-md:mt-10">
+            <div className="gap-5 flex max-md:flex-col max-md:items-stretch max-md:gap-0">
+              <div className="flex flex-col items-stretch w-6/12 max-md:w-full max-md:ml-0">
+                <div className="flex flex-col my-auto items-start max-md:max-w-full max-md:mt-10">
+                  <picture>
+                    <img
+                      loading="lazy"
+                      src="https://cdn.builder.io/api/v1/image/assets/TEMP/948451c9cd8add0c7dc0bef71b0473d97863fd278817d8c452b8ceeb18b02e5b?apiKey=052aa92796d54d509e3efd8cbd8b7989&"
+                      className="aspect-square object-contain object-center w-12 overflow-hidden max-w-full"
+                    />
+                  </picture>
+                  <div className="self-stretch text-black text-4xl font-bold leading-10 mt-6 max-md:max-w-full">
+                    Revolutionizing Underfloor Heating: The Inception of our
+                    Floorgrinder Technology
+                  </div>
+                  <div className="self-stretch text-black text-lg leading-7 mt-6 max-md:max-w-full">
+                    Our company has a rich history, starting with the
+                    development of the Floorgrinder technology, which has
+                    propelled us to become a leader in retrofit underfloor
+                    heating systems.
+                  </div>
+                </div>
+              </div>
+              <div className="flex flex-col items-stretch w-6/12 ml-5 max-md:w-full max-md:ml-0">
+                <picture>
+                  <img
+                    loading="lazy"
+                    srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/695e35fe9375ed813e9267c48b6e6e0ed2c5303c0a4eb46842040dcb8370ac1e?apiKey=052aa92796d54d509e3efd8cbd8b7989&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/695e35fe9375ed813e9267c48b6e6e0ed2c5303c0a4eb46842040dcb8370ac1e?apiKey=052aa92796d54d509e3efd8cbd8b7989&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/695e35fe9375ed813e9267c48b6e6e0ed2c5303c0a4eb46842040dcb8370ac1e?apiKey=052aa92796d54d509e3efd8cbd8b7989&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/695e35fe9375ed813e9267c48b6e6e0ed2c5303c0a4eb46842040dcb8370ac1e?apiKey=052aa92796d54d509e3efd8cbd8b7989&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/695e35fe9375ed813e9267c48b6e6e0ed2c5303c0a4eb46842040dcb8370ac1e?apiKey=052aa92796d54d509e3efd8cbd8b7989&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/695e35fe9375ed813e9267c48b6e6e0ed2c5303c0a4eb46842040dcb8370ac1e?apiKey=052aa92796d54d509e3efd8cbd8b7989&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/695e35fe9375ed813e9267c48b6e6e0ed2c5303c0a4eb46842040dcb8370ac1e?apiKey=052aa92796d54d509e3efd8cbd8b7989&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/695e35fe9375ed813e9267c48b6e6e0ed2c5303c0a4eb46842040dcb8370ac1e?apiKey=052aa92796d54d509e3efd8cbd8b7989&"
+                    className="aspect-[0.96] object-contain object-center w-full overflow-hidden grow max-md:max-w-full max-md:mt-10"
+                  />
+                </picture>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
